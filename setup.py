f499a8e3ef7e4114a5c3f9e7544a5bd632e7b303
Ismaïl BENHALLAM <ismailben44@gmail.com>
2024-06-03 01:12:56 +0100

    Create python-publish.yml

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
     },
     classifiers=[
         "Environment :: Console",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.11",
 )
